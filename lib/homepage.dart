ee0feac6443815fc9ed0f3d857713a1722cc2449
Just0lga <tkucukasci@gmail.com>
2025-03-12 23:06:07 +0300

    Homepage design

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,7 +11,37 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Flutter Demo Home Page')),
+      appBar: AppBar(
+        title: const Text('Welcome to Accounts Tracking'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+        child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  const Icon(Icons.add_a_photo, size: 48),
+                  const SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text('Account Name'),
+                      Text('Account Number'),
+
+                      Text('Account Name'),
+                      Text('Account Number'),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
